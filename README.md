a73fc69b59e880dcb484dc45316683d04a47b704
kemickoko <kemickokojiji.work@gmail.com>
2025-07-08 16:29:28 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# 開発環境設定
+
+## 環境変数ファイルの作成
+
+ルートディレクトリに、.env.devファイルを作成し、.env.exampleの中身をコピーして、貼り付けてください。（本番環境は.env.prod）
+
+``` bash
+cp .env.example .env.dev
+```
+
+中の変数のうち、your～となっている変数を自分で分かりやすいものに書き換えてください。
+
+## 開発環境の起動
+
+``` bash
+docker compose up
+```
+
+必要に応じて以下のオプションを追加
+
+``` bash
+--build # ビルドもする
+-d # バックグラウンド実行
+```
+
+## 本番環境の起動
+
+``` bash
+docker compose -f docker-compose.yml --profile production up
+```
+
+- docker-compose.ymlを指定して、本番環境のコンテナを起動。
+- nginx コンテナは profiles: ["production"] に設定しているため、開発環境では起動せず、本番環境でのみ起動するようにしています。
+- 本番環境では --profile production を指定して起動します。
+- 開発環境では docker compose up のみで nginx を除いたサービスが起動します。
---
+++ myproject/config/settings/prod.py
@@ -0,0 +1,35 @@
+import os
+from .base import *
+
+debug_env = os.getenv("DEBUG", "False").lower()
+if debug_env in ("true", "1", "yes"):
+    raise RuntimeError("DEBUG must be False in production!")
+
+DEBUG = False
+
+SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
+
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
+# RDS/MySQLなど本番DB設定
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT", "3306"),
+    }
+}
+
+# セキュリティ強化設定
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+STATIC_ROOT = '/app/static/'
+STATICFILES_DIRS = []
